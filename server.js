e811d7efb984d07100bafd3082cbc9f12a788632
Shaibya Dwivedi <shadwive@publicisgroupe.net>
2020-01-22 17:31:16 +0530

    Authentication code

--- server.js
+++ server.js
@@ -8,82 +8,13 @@ const app = express();
  redirect_uri = "https://sf-storm.herokuapp.com",
  consumer_secret = '2136BDFC61EAC9E4D29439647E609C01D99CBDDBF684EAC7F98A78FF3A54BCAC'
 
- app.all('/proxy',  function(req, res) {     
-    var url = req.header('SalesforceProxy-Endpoint');  
-    request({ url: url, method: req.method, json: req.body, 
-             headers: {'Authorization': req.header('X-Authorization'), 'Content-Type' : 'application/json'}, body:req.body }).pipe(res); 
+app.all('/proxy',  function(req, res) {     
+var url = req.header('SalesforceProxy-Endpoint');  
+request({ url: url, method: req.method, json: req.body, 
+            headers: {'Authorization': req.header('X-Authorization'), 'Content-Type' : 'application/json'}, body:req.body }).pipe(res); 
 });                
-
-function extractAccessToken(err, remoteResponse, remoteBody,res){
-	if (err) { 
-		return res.status(500).end('Error'); 
-	}
-	console.log(remoteBody) ;
-	var sfdcResponse = JSON.parse(remoteBody); 
-	
-	//success
-	if(sfdcResponse.access_token){				 
-		res.writeHead(302, {
-		  'Location': 'Main' ,
-		  'Set-Cookie': ['AccToken='+sfdcResponse.access_token,'APIVer='+apiVersion,'InstURL='+sfdcResponse.instance_url,'idURL='+sfdcResponse.id]
-		});
-	}else{
-		res.write('Some error occurred. Make sure connected app is approved previously if its JWT flow, Username and Password is correct if its Password flow. ');
-		res.write(' Salesforce Response : ');
-		res.write( remoteBody ); 
-	} 
-	res.end();
-}
-
-/**
- * Step 1 Web Server Flow - Get Code
- */
-app.get('/webServer1', function (req,res){  
-	var state = 'webServerProd';
-	var sfdcURL = 'https://login.salesforce.com/services/oauth2/authorize' ;
-    console.log('hello world');
-	request({ 	url : sfdcURL+'?client_id='+
-				 jwt_consumer_key+'&redirect_uri='+
-				 redirect_uri+'&response_type=code&state='+state,  
-				method:'GET' 
-			}).pipe(res);	 
-} );
-
-
-
-/**
- * Step 2 Web Server Flow - Get token from Code
- */
-app.get('/webServer', function (req,res){  
-    // var sfdcURL = 'https://login.salesforce.com/services/oauth2/token' ;
-    
-    //  request({ 	url : sfdcURL+'?client_id='+ jwt_consumer_key+'&redirect_uri='+redirect_uri+'&grant_type=authorization_code&code='+ 
-    //             req.query.code+'&client_secret'+consumer_secret,  
-	// 			method:'POST' 
-	// 		},
-	// 		function(err, remoteResponse, remoteBody) {
-	// 			extractAccessToken(err, remoteResponse, remoteBody, res); 
-	// 		} 
-    // 	);
-    // var sfdcURL = 'https://login.salesforce.com/services/oauth2/token' ;
-    // uname='monsieur.shaibya@gmail.com'
-    // pwd = 'Lawsonia_25'
-    // var computedURL = sfdcURL+
-	// '?client_id='+ '3MVG9_XwsqeYoueK3il2Xs5otrWbZv4V_6oUY1.v5lcO8YPk_iLjOOx0E7Jkofo8n_Zeoyq0ywAIC97aFBsNC'+
-	//  '&grant_type=password'+
-	//  '&client_secret='+'2136BDFC61EAC9E4D29439647E609C01D99CBDDBF684EAC7F98A78FF3A54BCAC'+
-	//  '&username='+uname+
-	//  '&password='+pwd ;
  
-
-	//  request({ 	url : computedURL,  
-	// 			method:'POST' 
-	// 		},
-	// 		function(err, remoteResponse, remoteBody) {
-	// 			extractAccessToken(err, remoteResponse, remoteBody, res); 
-	// 		} 
-    //     );  
-    
+app.get('/webServer', function (req,res){  
 	var sfdcURL = 'https://login.salesforce.com/services/oauth2/authorize' ;
 	request({ 	url : sfdcURL+'?client_id='+jwt_consumer_key+'&redirect_uri='+redirect_uri+'&response_type=token',  
 				method:'GET' 
@@ -101,4 +32,4 @@ app.get('/*', function(req,res) {
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-console.log('listning on' + process.env.PORT);
+ 
