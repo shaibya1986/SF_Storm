18895373456a6dc55fa0cf71e80f5028958947ad
Shaibya Dwivedi <shadwive@publicisgroupe.net>
2020-01-23 17:48:03 +0530

    Authentication code

--- src/app/getter-setter.ts
+++ src/app/getter-setter.ts
@@ -5,7 +5,7 @@ export class GetterSetter {
   private _sfdcURL = 'https://login.salesforce.com/services/oauth2/authorize';
   private _endpoint = 'https://login.salesforce.com/services/oauth2/token';
   private _salesForceURL = 'https://sapient-shaibya-dev-ed--c.visualforce.com/services/data/v42.0/query/?q=SELECT+position__c,category__c,date_posted__c,body__c,Name+FROM+Post__c';
- '
+ 
   get clientId() {
     return this._clientId;
   }
